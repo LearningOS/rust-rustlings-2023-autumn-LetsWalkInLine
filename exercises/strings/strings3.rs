6ac622e55021cddab804401b94d9e0527ec42efe
LetsWalkInLine <121844090+LetsWalkInLine@users.noreply.github.com>
2023-10-11 13:53:50 +0000

    modified:   exercises/strings/strings3.rs 	modified:   exercises/strings/strings4.rs

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
@@ -17,7 +16,7 @@ fn compose_me(input: &str) -> String {
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    input.replace()
+    input.to_string().replace("cars", "balloons")
 }
 
 #[cfg(test)]
