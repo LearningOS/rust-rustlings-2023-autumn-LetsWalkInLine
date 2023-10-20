f33a9540f70e0655041c6b9b8f15cc9c375ac127
LetsWalkInLine <121844090+LetsWalkInLine@users.noreply.github.com>
2023-10-20 07:52:28 +0000

    modified:   exercises/smart_pointers/arc1.rs 	modified:   exercises/smart_pointers/box1.rs 	modified:   exercises/smart_pointers/cow1.rs 	modified:   exercises/smart_pointers/rc1.rs 	modified:   exercises/threads/threads1.rs

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -8,7 +8,7 @@
 // Execute `rustlings hint threads1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::thread;
 use std::time::{Duration, Instant};
@@ -24,7 +24,7 @@ fn main() {
         }));
     }
 
-    let mut results: Vec<u128> = vec![];
+    let mut results: Vec<u128> = vec![0;10];
     for handle in handles {
         // TODO: a struct is returned from thread::spawn, can you use it?
     }
