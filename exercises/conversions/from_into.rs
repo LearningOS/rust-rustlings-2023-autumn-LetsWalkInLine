c27cc21b9bfcb1c9305c8f17d7d5f4c196220af1
LetsWalkInLine <121844090+LetsWalkInLine@users.noreply.github.com>
2023-10-20 15:09:18 +0000

    modified:   exercises/clippy/clippy1.rs 	modified:   exercises/clippy/clippy2.rs 	modified:   exercises/clippy/clippy3.rs 	modified:   exercises/conversions/as_ref_mut.rs 	modified:   exercises/conversions/from_into.rs 	modified:   exercises/conversions/from_str.rs 	modified:   exercises/conversions/try_from_into.rs 	modified:   exercises/conversions/using_as.rs

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,27 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
+
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        } else {
+            let v: Vec<&str> = s.split(',').collect();
+            if v.len() != 2 || v[0].len() == 0 || v[1].len() == 0 {
+                return Person::default();
+            } else {
+
+                return Person {
+                    name: v[0].to_string(),
+                    age: match v[1].parse::<usize>() {
+                        Ok(n) => n,
+                        _ => return Person::default(),
+                    }
+                };
+            }
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,7 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
+
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +52,27 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        } else {
+            let v: Vec<&str> = s.split(',').collect();
+
+            if v[0].len() == 0 {
+                return Err(ParsePersonError::NoName);
+            }
+
+            if v.len() != 2 {
+                return Err(ParsePersonError::BadLen);
+            } else {
+                return Ok(Person {
+                    name: v[0].to_string(),
+                    age: match v[1].parse() {
+                        Ok(n) => n,
+                        Err(e) => return Err(ParsePersonError::ParseInt(e)),
+                    }
+                });
+            }
+        }
     }
 }
 
