66dd6d0f9ca2a2b1c679113c5356ec1b2cd1d19e
LetsWalkInLine <121844090+LetsWalkInLine@users.noreply.github.com>
2023-10-20 13:41:58 +0000

    modified:   exercises/macros/macros1.rs 	modified:   exercises/macros/macros2.rs 	modified:   exercises/macros/macros3.rs 	modified:   exercises/macros/macros4.rs 	modified:   exercises/threads/threads2.rs 	modified:   exercises/threads/threads3.rs

--- exercises/threads/threads2.rs
+++ exercises/threads/threads2.rs
@@ -7,7 +7,7 @@
 // Execute `rustlings hint threads2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::sync::Arc;
 use std::thread;
@@ -25,7 +25,7 @@ fn main() {
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
             // TODO: You must take an action before you update a shared value
-            status_shared.jobs_completed += 1;
+            //status_shared.jobs_completed += 1;
         });
         handles.push(handle);
     }
@@ -34,6 +34,6 @@ fn main() {
         // TODO: Print the value of the JobStatus.jobs_completed. Did you notice
         // anything interesting in the output? Do you have to 'join' on all the
         // handles?
-        println!("jobs completed {}", ???);
+        println!("jobs completed {}", 0);
     }
 }
--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::sync::mpsc;
 use std::sync::Arc;
@@ -26,15 +26,17 @@ impl Queue {
     }
 }
 
-fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
-    let qc = Arc::new(q);
-    let qc1 = Arc::clone(&qc);
-    let qc2 = Arc::clone(&qc);
+fn send_tx(q: Arc<Queue>, tx: mpsc::Sender<u32>) -> () {
+    let qc1 = Arc::clone(&q);
+    let qc2 = Arc::clone(&q);
+
+    let tx1 = tx.clone();
+    let tx2 = tx.clone();
 
     thread::spawn(move || {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx1.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
@@ -42,7 +44,7 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     thread::spawn(move || {
         for val in &qc2.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx2.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
@@ -53,7 +55,7 @@ fn main() {
     let queue = Queue::new();
     let queue_length = queue.length;
 
-    send_tx(queue, tx);
+    send_tx(Arc::new(queue), tx);
 
     let mut total_received: u32 = 0;
     for received in rx {
